5b2c44833ae6b7f8a8001ac622f6386d815fbc47
orekamenpe <orekamenpe@gmail.com>
2016-01-13 14:23:53 +0100

    added scripts for the ball and rackets

---
+++ Pong/Assets/MoveRacket.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    public float speed = 30;
+    public string axis = "Vertical";
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+        rigidBody.velocity = new Vector2(0, v) * speed;
+    }
+}
